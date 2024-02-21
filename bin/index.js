6b4a856514e1762df8ecfba8e337dd51435ad269
rohansx <sharmarohan615@gmail.com>
2024-02-22 00:23:19 +0530

    update style

--- bin/index.js
+++ bin/index.js
@@ -13,28 +13,47 @@ const options = {
 };
 
 const data = {
-  intro:
-    "I am Rohan, your friendly neighborhood coder, focused on helping future developers be better!",
-  work: "Associate Software Developer at OpenXYZ",
-  twitter: "https://twitter.com/rohaxyz",
-  github: "https://github.com/rohansx",
-  linkedin: "https://linkedin.com/in/rohansx",
-  web: "https://rohan.sh",
+  intro: chalk.white.bold(
+    "I am Rohan, your friendly neighborhood coder, focused on helping future developers be better!"
+  ),
+  work: chalk.white("Associate Software Developer") + chalk.blue("OpenXYZ"),
+  twitter: chalk.gray("https://twitter.com/") + chalk.cyan("rohaxyz"),
+  github: chalk.gray("https://github.com/") + chalk.green("rohansx"),
+  linkedin: chalk.gray("https://linkedin.com/in/") + chalk.blue("rohansx"),
+  //   youtube: chalk.gray("https://youtube.com/") + chalk.red("@rohansx"),
+  web: chalk.cyan.underline("https://rohan.sh"),
+  labelWork: chalk.bgWhite.black.bold("       Work"),
+  labelTwitter: chalk.bgWhite.black.bold("    Twitter"),
+  labelGitHub: chalk.bgWhite.black.bold("     GitHub"),
+  labelLinkedIn: chalk.bgWhite.black.bold("   LinkedIn"),
+  labelYouTube: chalk.bgWhite.black.bold("   YouTube"),
+  labelWeb: chalk.bgWhite.black.bold("        Web"),
 };
 
-const label = (title) => chalk.bgWhite.black.bold(` ${title} `);
-const url = (url, color) => chalk[color].underline(url);
-const line = (label, content, color = "white") =>
-  `${label}  ${chalk[color](content)}`;
+const newline = "\n";
+const introduction = `${data.intro}`;
+const working = `${data.labelWork}  ${data.work}`;
+const twittering = `${data.labelTwitter}  ${data.twitter}`;
+const githubing = `${data.labelGitHub}  ${data.github}`;
+const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`;
+const youtubing = `${data.labelYouTube}  ${data.youtube}`;
+const webing = `${data.labelWeb}  ${data.web}`;
 
-const output = [
-  data.intro,
-  "",
-  line(label("Work"), data.work),
-  line(label("Twitter"), url(data.twitter, "cyan")),
-  line(label("GitHub"), url(data.github, "green")),
-  line(label("LinkedIn"), url(data.linkedin, "blue")),
-  line(label("Web"), url(data.web, "cyan")),
-].join("\n");
+const output =
+  introduction +
+  newline +
+  newline +
+  newline +
+  working +
+  newline +
+  twittering +
+  newline +
+  githubing +
+  newline +
+  linkedining +
+  newline +
+  youtubing +
+  newline +
+  webing;
 
 console.log(chalk.white(boxen(output, options)));
